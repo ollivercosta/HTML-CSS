932345df73cd6bb58d570a429691dff85762de22
Josafá <150770417+ollivercosta@users.noreply.github.com>
2023-11-24 21:29:27 -0300

    exercicio form01

---
+++ exercicio025/cadastro.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "<p>Minha primeira página em PHP</p>"; ?>
+</body>
+</html>
